bfad4a3f36346c2070510d8c386c861719daf22d
Nicolas Andretta <nicolasandretta@gmail.com>
2024-12-02 22:30:09 -0300

    Agregando cartwidget

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
+import CartWidget from '../CartWidget';
 
 export const NavBar = () => {
-  return (
-    <div className="container">
-        <nav className="nav">
-            <div className="nav_brand">
-                <a className="nav_link" href="#">Mitienda</a>
-            </div>
-            <ul className="nav_list"></ul>
-            <li>
-                <a className="nav_link" href="#"></a>producto1</li>
-            <li>
-                <a className="nav_link" href="#"></a>producto2</li>
-                <li>
-                    <a className="nav_link" href="#">carrito</a>
-                </li>
-        </nav>
-    </div>
-  );
+    return (
+        <div className="container">
+            <nav className="nav">
+                <div className="nav_brand">
+                    <a className="nav_link" href="#">Mitienda</a>
+                </div>
+                <ul className="nav_list">
+                    <li>
+                        <a className="nav_link" href="#">producto1</a>
+                    </li>
+                    <li>
+                        <a className="nav_link" href="#">producto2</a>
+                    </li>
+                    <li>
+                        <a className="nav_link" href="#">
+                            <CartWidget /> 
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 };
 
 export default NavBar;
+
